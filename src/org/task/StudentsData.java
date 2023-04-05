a352b081940023f1ecf295866c4475e7980c02e9
Lavanyabalu <lavanyabalu033@gmail.com>
2023-04-05 23:20:59 +0530

    lava1

---
+++ src/org/task/StudentsData.java
@@ -0,0 +1,23 @@
+package org.task;
+
+
+public class StudentsData {
+	
+	
+	public void studentName() {
+		System.out.println("Student Name :Lachu");
+		}
+	
+	public void studentId() {
+	System.out.println("Student id:123434");
+	}
+	
+	public static void main(String[] args) {
+		
+		StudentsData e=new StudentsData();
+		e.studentName();
+		e.studentId();
+	}
+	
+
+}
